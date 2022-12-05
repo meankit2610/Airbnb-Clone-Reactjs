7f54defd0be92e09b095ae8ccc518b6ae8765ff3
meankit2610 <ankitsingh5282770@gmail.com>
2022-12-05 13:18:04 +0530

    created structure

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from "../../assets/"
+const Header = () => {
+  return (
+    <div className="navbar">
+          <img src{logo} className="navbar-logo" />
+    </div>
+  )
+}
+
+export default Header
