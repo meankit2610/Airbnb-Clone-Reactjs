3e98b98851a3a0e0685525f9e63ccb5357b52de0
meankit2610 <ankitsingh5282770@gmail.com>
2022-12-07 21:44:57 +0530

    filter-bar Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Filter from "./components/Filter";
 import Header from "./components/Header";
 
 function App() {
   return <div className="App">
-    <Header/>
+    <Header />
+    <Filter/>
   </div>;
 }
 
---
+++ src/components/Filter/index.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import { links } from '../../assets/images-links'
+import './styles.css'
+const Filter = () => {
+    const [selectedFilter, setSelectedFilter] = useState()
+  return (
+    <div className='filter-div'>
+          {links.map((item, i) => (
+              <div key={i} className={`links-box ${i==selectedFilter && 'selected-box'}`}
+                  onClick={() => {
+                  console.log("selecting",i)
+                  setSelectedFilter(i)
+              }}>
+                  <img src={item.imgSrc} className='links-img' />
+                  <p className={`links-label ${i==selectedFilter && 'selected-label'}`}>{ item.label }</p>
+          </div>
+      ))}
+    </div>
+  )
+}
+
+export default Filter
